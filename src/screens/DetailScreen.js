8dc28a118e7e0a68e8aa8f28d63655552bc36e48
Lee <s110919016@stu.ntue.edu.tw>
2022-03-22 11:00:38 +0800

    detailscreen

--- src/screens/DetailScreen.js
+++ src/screens/DetailScreen.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import { Linking } from 'react-native';
-import { Center, ScrollView, Box, AspectRatio, Text, Heading, Image, Button } from "native-base";
+import { Center, ScrollView, Box, AspectRatio, Text, Heading, Image, Button, VStack,HStack } from "native-base";
 
 
 const DetailScreen = ({ route }) => {
   const { title, 
     artist,
     price,
+    score,
+    fullscore,
     url,
     image,
     description
   } = route.params;
   return (
-    <Center>
+    <Center bgColor="#fff" flex={1}>
       <ScrollView>
         <Center>
           <Image w={210} h={300}
@@ -20,30 +22,34 @@ const DetailScreen = ({ route }) => {
             alt='albumImage'
           />
         </Center>
-        <Box bg="#fff" padding="2" margin="2">
-          <Center>
-            <Heading pt={1} fontSize="2xl" color='#6099E4'>Discount Now!</Heading>
-            <Heading fontSize="4xl">Price: ${price}</Heading>
-          </Center>
+        <Center mt="28px">
+            <Text fontSize="24px" fontWeight={"bold"}>
+                {artist}
+            </Text>
+            <Text fontSize="14px" fontWeight={"bold" } color="#929292">             
+                {title}
+            </Text>
+        </Center>
+        <Center>
+          <HStack>
+            <Heading pt={1} fontSize="14px" color='#6099E4'>Discount Now!</Heading>
+            <Heading pt={1} fontSize="14px">{'\t'}{score}<Text  ml="13px" color="#929292">{'\t'}/</Text><Text color="#929292">{'\t'}{fullscore}</Text></Heading>
+          </HStack>
+          <Box  mt="16px" ml="20px" mr="20px" w={320}>
+            <Text lineHeight="24px" letterSpacing="0.018em" textAlign="center">{description}</Text>
+          </Box>
           <Button 
-            mt="4"
+            borderRadius="4px"
+            mt="28px"
+            w="190"
+            h="36"
+            bgColor="#6200EE"
             onPress={() => Linking.openURL(url)}
           >
-            Buy Now !
+            <Text color="#fff" fontSize="14px" letterSpacing="0.08em">BUY NOW FOR ${price}</Text>
           </Button>   
-        </Box>
-        <Box bg="#fff" padding="2" margin="2">
-            <Text>
-              <Text bold>Artist: </Text>
-              {artist}
-            </Text>
-            <Text>            
-              <Text bold>Title: </Text>
-              {title}
-            </Text>
-            <Text mt='15' bold>Descriptions:</Text>
-            <Text>{'\t'}{description}</Text>
-        </Box>
+        </Center>
+        
       </ScrollView>      
     </Center>
 
